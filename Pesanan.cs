b6c5343003934bd461c220f6cd7b256143ba981e
deboratan <ifs15005@students.del.ac.id>
2019-05-27 13:42:23 +0700

    add model

--- Pesanan.cs
+++ Pesanan.cs
@@ -3,21 +3,73 @@ using System.ComponentModel.DataAnnotations;
 
 namespace papikos.Models
 {
-    public class Pesanan
+     public class Pesanan
     {   
-        // [Display(Name="No")]
         public int IdPesanan{get;set;}
         
-         // [Display(Name="No")]
         public int IdRenter{get;set;}
 
-         // [Display(Name="No")]
         public int IdKamar{get;set;}
 
-         // [Display(Name="No")]
         public int IdKos{get;set;}
 
-         // [Display(Name="No")]
         public int NamaKos{get;set;}
+
+        public List<Renter> Renters {get; set;}
+        public List<Kamar> Kamars {get; set;}
+        public List<Kos> Koss {get; set;}
     }
+
+
+     public class Renter
+    {   
+        public int IdRenter{get;set;}
+        
+        public string NamaRenter{get;set;}
+
+        public string NoTelepon{get;set;}
+
+        public string Alamat{get;set;}
+
+        public string Email{get;set;}
+
+        public int IdPesanan { get; set; }
+   		public Pesanan Pesanan { get; set; }
+    }
+
+     public class Kamar
+    {   
+
+        public int IdKamar{get;set;}
+
+        public string Gambar{get;set;}
+
+        public string UkuranKamar{get;set;}
+
+        public string HargaKamar{get;set;}
+
+        public string StatusKamar{get;set;}
+
+        public string Alamat{get;set;}
+
+        public int IdPesanan { get; set; }
+   		public Pesanan Pesanan { get; set; }
+    }
+
+     public class Kos
+    {   
+
+        public int Id{get;set;}
+
+        public string NamaKos{get;set;}
+
+        public int JumlahKamar{get;set;}
+
+        public string  DeskripsiTambahan{get;set;}
+
+        public int IdPesanan { get; set; }
+   		public Pesanan Pesanan { get; set; }
+    }
+
+
 }
